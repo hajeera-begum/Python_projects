0dde7c434cbd6f18ead5e8c4b5094a040cd27abe
Hajeera <hajeerajobs@gmail.com>
2025-07-13 15:23:01 -0700

    Update caesar_cipher.py
    Merged encode and decode function. Added a while loop for continuing the program

--- Caesar_Cipher/caesar_cipher.py
+++ Caesar_Cipher/caesar_cipher.py
@@ -4,35 +4,26 @@ print(art.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-def encrypt(shift):
+def caesar(text, shift_amount, encode_or_decode):
     message=""
+    if encode_or_decode=="decode":
+        shift_amount*=-1
     for letter in text:
-        if letter in range(len(alphabet)-1):
-            encode=alphabet.index(letter)+shift
-            message+=alphabet[encode]
+        if letter in alphabet:
+            index=(alphabet.index(letter))
+            new_index = (index+shift_amount)%26
+            message+=alphabet[new_index]
         else:
-            encode=(alphabet.index(letter)+shift)%26
-            message += alphabet[encode]
-    print(f"Encoded Message:{message}")
+            message += letter
+    print(f"{encode_or_decode}d Message:{message}")
 
-def decrypt(shift):
-    message=""
-    for letter in text:
-        if letter in range(len(alphabet)-1):
-            decode=alphabet.index(letter)-shift
-            message+=alphabet[decode]
-        else:
-            decode=(alphabet.index(letter)-shift)%26
-            message += alphabet[decode]
-    print(f"Decoded Message:{message}")
-
-if direction=="encode":
-    encrypt(shift)
-elif  direction=="decode":
-    decrypt(shift)
-else:
-    print("Please give the correct input.")
+should_continue=True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(text,shift,direction)
+    repeat=input("Would you like to continue? Type 'Yes' or 'No'").lower()
+    if repeat=="no":
+        print("Thank You for your visit\nGood Bye")
+        should_continue=False
